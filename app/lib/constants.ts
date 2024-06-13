dc6b1c6d959d34fea20a9b75a957e2d3919f59c4
ankahara <hecatombe100@gmail.com>
2024-06-13 17:54:36 -0600

    correcion

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -2,7 +2,7 @@ import BG_HOME_DESKTOP from "@/public/background/bg-home.jpg";
 import BG_HOME_DESTINATION from "@/public/background/bg-destination.jpg";
 import BG_CREW_DESKTOP from "@/public/background/bg-crew.png";
 import BG_CREW_TECHNOLOGY from "@/public/background/bg-technology.png";
-import LOGO from "@/public/logo.svg";
+import LOGO from "@/public/Logo.svg";
 export const BACKGROUNDS = {
     HOME: {
         desktop: BG_HOME_DESKTOP
